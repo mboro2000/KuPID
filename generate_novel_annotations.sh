909fd62aebe1c1818d08aa522910c14591e01109
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 12:46:04 -0400

    Update generate_novel_annotations.sh

--- generate_novel_annotations.sh
+++ generate_novel_annotations.sh
@@ -11,6 +11,7 @@ wget https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_48/gencod
 #release 48 of human genome via GenCode
 
 mkdir $1/novel_isoform_data
+#cd to yasim folder
 python -m yasim generate_as_events -f $1/reference_data/GRCh38.primary_assembly.genome.fa -g $1/reference_data/gencode.v48.annotation.gtf -o $1/novel_isoform_data/gencode.v48.as.gtf -c 2
 python -m yasim generate_gene_depth -g $1/novel_isoform_data/gencode.v48.as.gtf -o $1/novel_isoform_data/gencode.v48.gene_depth.tsv -d 20
 python -m yasim generate_isoform_depth -g $1/novel_isoform_data/gencode.v48.as.gtf -d $1/novel_isoform_data/gencode.v48.gene_depth.tsv -o $1/novel_isoform_data/gencode.v48.isoform_depth.tsv
