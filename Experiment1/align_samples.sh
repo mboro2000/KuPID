55d8627b87b362edf0e7023ee803cbe50d20c5a9
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 12:44:48 -0400

    Update align_samples.sh

--- Experiment1/align_samples.sh
+++ Experiment1/align_samples.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-mkdir Experiment1/minimap2_output
-cd "/usr1/mborowia/minimap2" || { echo "Directory not found!"; exit 1; }
+#arg1 = main directory
+
+cd "$1/minimap2" || { echo "Directory not found!"; exit 1; }
 
 for s in 20 40 60 80 100;do
 ./minimap2 -ax splice --MD /usr1/mborowia/human_p14_chr_1_22.fa /usr1/mborowia/Experiment1/novel_samples/novel_$s%.css.fa -t 3 -o /usr1/mborowia/Experiment1/minim>#       samtools view -S -b /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.sam > /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.bam
