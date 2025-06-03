214d22cbab89e5d6b5bda365f89e5ca30af4141f
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 12:53:04 -0400

    Update align_samples.sh

--- Experiment1/align_samples.sh
+++ Experiment1/align_samples.sh
@@ -15,7 +15,11 @@ bedtools bamtobed -bed12 -i $1/Experiment1/minimap2_output/novel_$s%.bam > $1/Ex
 done
 
 for a in 0 1 2 5 10;do
-./minimap2 -ax splice --MD /usr1/mborowia/human_p14_chr_1_22.fa /usr1/mborowia/Experiment1/ratio_samples/1:$a.css.fa -t 3 -o /usr1/mborowia/Experiment1/minimap2_ou>        samtools view -S -b /usr1/mborowia/Experiment1/minimap2_output/ratio_1:$a.sam > /usr1/mborowia/Experiment1/minimap2_output/ratio_1:$a.bam
-samtools sort /usr1/mborowia/Experiment1/minimap2_output/ratio_1:$a.bam -o /usr1/mborowia/Experiment1/minimap2_output/ratio_1:$a.sorted.bam
-samtools index /usr1/mborowia/Experiment1/minimap2_output/ratio_1:$a.sorted.bam
+./minimap2 -ax splice --MD $1/reference_data/GRCh38.primary_assembly.genome.fa $1/Experiment1/ratio_samples/ratio_1:$a.css.fa -t 3 -o $1/Experiment1/minimap2_output/ratio_1:$a.sam      
+	
+samtools view -S -b $1/Experiment1/minimap2_output/ratio_1:$a.sam > $1/Experiment1/minimap2_output/ratio_1:$a.bam
+samtools sort $1/Experiment1/minimap2_output/ratio_1:$a.bam -o $1/Experiment1/minimap2_output/ratio_1:$a.sorted.bam
+samtools index $1/Experiment1/minimap2_output/ratio_1:$a.sorted.bam
+bedtools bamtobed -bed12 -i $1/Experiment1/minimap2_output/ratio_1:$a.bam > $1/Experiment1/minimap2_output/ratio_1:$a.bed12
+
 done
