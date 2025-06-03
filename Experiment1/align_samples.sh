9a4dc6187911896b327946587c8aa0cad38df9a3
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 12:50:52 -0400

    Update align_samples.sh

--- Experiment1/align_samples.sh
+++ Experiment1/align_samples.sh
@@ -5,9 +5,13 @@
 cd "$1/minimap2" || { echo "Directory not found!"; exit 1; }
 
 for s in 20 40 60 80 100;do
-./minimap2 -ax splice --MD /usr1/mborowia/human_p14_chr_1_22.fa /usr1/mborowia/Experiment1/novel_samples/novel_$s%.css.fa -t 3 -o /usr1/mborowia/Experiment1/minim>#       samtools view -S -b /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.sam > /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.bam
-samtools sort /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.bam -o /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.sorted.bam
-samtools index /usr1/mborowia/Experiment1/minimap2_output/novel_$s%.sorted.bam
+./minimap2 -ax splice --MD $1/reference_data/GRCh38.primary_assembly.genome.fa $1/Experiment1/novel_samples/novel_$s%.css.fa -t 3 -o $1/Experiment1/minimap2_output/novel_$s%.sam      
+	
+samtools view -S -b $1/Experiment1/minimap2_output/novel_$s%.sam > $1/Experiment1/minimap2_output/novel_$s%.bam
+samtools sort $1/Experiment1/minimap2_output/novel_$s%.bam -o $1/Experiment1/minimap2_output/novel_$s%.sorted.bam
+samtools index $1/Experiment1/minimap2_output/novel_$s%.sorted.bam
+bedtools bamtobed -bed12 -i $1/Experiment1/minimap2_output/novel_$s%.bam > $1/Experiment1/minimap2_output/novel_$s%.bed12
+
 done
 
 for a in 0 1 2 5 10;do
