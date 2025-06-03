348d69d7ae73e23b493287cf52780df162dd9ad6
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 12:54:35 -0400

    Update main.sh

--- Experiment1/main.sh
+++ Experiment1/main.sh
@@ -5,3 +5,5 @@ mkdir $1/Experiment1
 ./sample_files.sh $1/novel_isoform_data/novel.ccs.bam $1/novel_isoform_data/annotated.ccs.bam $1/Experiment1
 
 mkdir $1/Experiment1/minimap2_output
+./align_samples.sh $1
+./analyze.sh $1
