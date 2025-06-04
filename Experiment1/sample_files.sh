4806820f072ca8bb61cf3d32c6c965b920f63448
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-03 23:00:29 -0400

    Update sample_files.sh

--- Experiment1/sample_files.sh
+++ Experiment1/sample_files.sh
@@ -5,12 +5,12 @@
 #arg3: main directory (/usr1/mborowia)
 
 # Print a welcome message
-mkdir novel_samples
+mkdir $3/Experiment1/novel_samples
 for s in 20 40 60 80 100;do
 python $3/Experiment1/novel_sample.py $1 $s "$3/Experiment1/novel_samples"
 done
 
-mkdir ratio_samples
+mkdir $3/Experiment1/ratio_samples
 for a in 0 1 2 5 10;do
 python $3/Experiment1/ratio_sample.py $1 $2 $s "$3/Experiment1/ratio_samples"
 done
