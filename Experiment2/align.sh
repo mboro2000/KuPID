a1ccb2f3eaf96f4175adc55d9ae8270cb122b96a
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-04 20:27:40 -0400

    Update align.sh

--- Experiment2/align.sh
+++ Experiment2/align.sh
@@ -7,7 +7,7 @@ mkdir $1/Experiment2/minimap2_output
 
 cd "$1/minimap2" || { echo "Directory not found!"; exit 1; }
 
-./minimap2 -ax splice --MD $1/reference_data/GRCh38.primary_assembly.genome.fa $1/novel_isoform_data/all.ccs.fa -t 3 -o $1/Experiment2/minimap2_output/all.ccs.sam
+./minimap2 -ax splice --MD $1/reference_data/GRCh38.chr1-22.fa $1/novel_isoform_data/all.ccs.fasta -t 3 -o $1/Experiment2/minimap2_output/all.ccs.sam
 
 samtools view -S -b $1/Experiment2/minimap2_output/all.ccs.sam > $1/minimap2_output/all.ccs.bam
 samtools sort $1/Experiment2/minimap2_output/all.ccs.bam -o $1/Experiment2/minimap2_output/all.ccs.sorted.bam
