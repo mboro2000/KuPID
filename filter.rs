b2a3c57590afaadf2d5274561b8316b9173dbd66
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2025-06-16 16:50:11 -0400

    Update bin subsampling procedure

--- filter.rs
+++ filter.rs
@@ -1,70 +1,88 @@
 use std::collections::HashMap;
-
+use std::sync::Mutex;
+use crate::types::*;
+use std::sync::{Arc, RwLock};
 use crate::types;
 use std::path::Path;
 use std::fs;
 
-pub fn filter(sample_data:&Vec<HashMap<String, String>>, ref_map:HashMap<String, Vec<types::Match>>, output:String, nb:i32, m:i32, bin_s:f32, s:f64){
+pub fn filter(sample_data_shared:Arc<RwLock<Vec<HashMap<String, String>>>>, ref_map_shared:Arc<RwLock<HashMap<String, Vec<Match>>>>, nb:i32, bin_s:f32, s:f64, num_exon_gaps:usize)
+-> HashMap<String, usize>{
 
-    let candidates_path = Path::new(&output);
-    let mut candidates_outline = "".to_string();
+    
+    let sample_data = sample_data_shared.read().unwrap();
+    let ref_map = ref_map_shared.read().unwrap();
 
-    let mut num_exon_gaps = 0;
-    let total_reads = sample_data.len();
+    //let mut num_exon_gaps = 0;
+    let mut total_reads = 0;
+    for chunk in sample_data.iter(){
+        total_reads += chunk.len();
+    }
+    //println!("Total reads is {}", total_reads);
     let mut sim_scores:Vec<i32> = Vec::new();
     let mut all_sim_scores:Vec<i32> = Vec::new();
     let mut subsampled_reads:Vec<types::Match> = Vec::new();
 
-    println!("Num. of Isoforms in Map: {}", ref_map.len());
+    //println!("Num. of Isoforms in Map: {}", ref_map.len());
     let mut i_count = 0;
 
+    let mut criteria_2:HashMap<String, usize> = HashMap::new();
+
     for (ref_id, sample_matches) in ref_map.iter(){
-        i_count += 1;
-        println!("{}", i_count);
+        
         let mut bins:Vec<types::Bin> = Vec::new();
         for i in 1 .. nb+1{     
             bins.push(types::build_Bin(((i as f32) / nb as f32), Vec::new(), false, 2.0, 0, 0, Vec::new()))
         }
 
-        for samp_match in sample_matches.iter(){
-            if samp_match.gap > m{
-                num_exon_gaps += 1;
-                candidates_outline.push_str(&(samp_match.sample_id.clone() + "\n" + &sample_data[samp_match.chunk].get(&samp_match.sample_id).expect("msg") + "\n"));
-            }
-            else{
-                for read in sample_matches{
-                    for i in 1 .. (nb+1) as usize{
-                        if read.similarity >= ((i as f32 - 1.0) / nb as f32) && read.similarity < ((i as f32) / nb as f32){
-                            /*
+        for read in sample_matches.iter(){
+            for i in 1 .. (nb+1) as usize{
+                if (read.similarity as f32 / 10000.0) >= ((i as f32 - 1.0) / nb as f32) && (read.similarity as f32 / 10000.0) < ((i as f32) / nb as f32){
+                           /*
                             if read.read_type == "novel"{
                                 bins[i-1].contain_novel = true;
                                 bins[i-1].num_novel += 1;
                             }
                              */
                             
-                            bins[i-1].num_total += 1;
-                            if read.similarity < bins[i-1].min_val{
-                                bins[i-1].min_val = read.similarity;
-                            }       
-                            bins[i-1].scores.push((10000.0 * read.similarity) as i32);
-                            bins[i-1].matches.push(read.clone());
-                        break;  //
-                        }   
-                    }
-                }
+                    bins[i-1].num_total += 1;
+                    /*
+                    if read.similarity < bins[i-1].min_val{
+                        bins[i-1].min_val = read.similarity;
+                    }   
+                     */
+                        
+                    //bins[i-1].scores.push((10000.0 * read.similarity) as i32);
+                    bins[i-1].scores.push(read.similarity);
+                    bins[i-1].matches.push(read.clone());
+                }   
             }
         }
+            
         for bin in bins{
             if bin.num_total > 0{
                 let mut bin_scores = bin.scores;
                 bin_scores.sort();
-                let subsample = bin_scores[((bin.num_total as f32 * bin_s).ceil() as usize - 1)] as f32 / 10000.0;
+                let bin_sample = ((bin_scores.len() as f32 * bin_s).ceil() as usize);
+                let mut selected = 0;
+                //println!("{:#?}", bin_scores);
+                let mut subsample = -1;
+                if bin_s > 0.0{
+                    //subsample = bin_scores[((bin.num_total as f32 * bin_s).ceil() as usize - 1)] as f32 / 10000.0;
+                    //subsample = bin_scores[((bin_scores.len() as f32 * bin_s).ceil() as usize) - 1] as f32 / 10000.0;
+                    subsample = bin_scores[bin_sample - 1];
+                }
+                //println!("subsample is {}", subsample);
 
                 for read in bin.matches{
-                    all_sim_scores.push((10000.0 * read.similarity) as i32);
-                    if read.similarity <= subsample{
-                        sim_scores.push((10000.0 * read.similarity) as i32);
+                    //all_sim_scores.push((10000.0 * read.similarity) as i32);
+                    all_sim_scores.push(read.similarity);
+                    //if read.similarity <= subsample && selected < bin_sample{     //t1
+                    if read.similarity <= subsample{     //t2
+                        //sim_scores.push((10000.0 * read.similarity) as i32);
+                        sim_scores.push(read.similarity);
                         subsampled_reads.push(read.clone());
+                        selected += 1;
                     }
                 }
             }
@@ -75,28 +93,52 @@ pub fn filter(sample_data:&Vec<HashMap<String, String>>, ref_map:HashMap<String,
 
     sim_scores.sort();
     all_sim_scores.sort();
-    let mut threshold_collapse = 0.0;
-    let threshold_no_collapse = all_sim_scores[cand_to_select as usize] as f32 / 10000.0;
+    //let mut threshold_collapse = -1.0;
+    //let threshold_no_collapse = all_sim_scores[cand_to_select as usize] as f32 / 10000.0;
 
-    if cand_to_select >= sim_scores.len() as f64{
-        threshold_collapse = *sim_scores.last().expect("msg") as f32 / 10000.0;
-    }
-    else{
-        threshold_collapse = sim_scores[cand_to_select as usize] as f32 / 10000.0;
+    let mut threshold_collapse = -1;
+    let threshold_no_collapse = all_sim_scores[cand_to_select as usize];
+
+   // println!("{}", sim_scores.len());
+    //println!("{}", all_sim_scores.len());
+
+
+    if bin_s > 0.0{
+        if cand_to_select >= sim_scores.len() as f64{
+            //threshold_collapse = *sim_scores.last().expect("msg") as f32 / 10000.0;
+            threshold_collapse = *sim_scores.last().expect("msg");
+        }
+        else{
+            //threshold_collapse = sim_scores[cand_to_select as usize] as f32 / 10000.0;
+            threshold_collapse = sim_scores[cand_to_select as usize];
+        }
     }
     
-    println!("{}", num_exon_gaps);              //1972
-    println!("Cand to select: {}", cand_to_select);             //46292.5
-    println!("Collapse threshold is {}", threshold_collapse);
-    println!("No Collapse threshold is {}", threshold_no_collapse);
+    
+    //println!("{}", num_exon_gaps);              //1972
+    //println!("Cand to select: {}", cand_to_select);             //46292.5
+    //println!("Collapse threshold is {}", threshold_collapse  as f32 / 10000.0);
+    //println!("No Collapse threshold is {}", threshold_no_collapse  as f32 / 10000.0);
 
+    let mut crit_2_novel = 0;
+    let mut crit_2_annot = 0;
 
     for read in subsampled_reads{
         if read.similarity <= threshold_collapse{
-            candidates_outline.push_str(&(read.sample_id.clone() + "\n" + &sample_data[read.chunk].get(&read.sample_id).expect("msg") + "\n"));
+            if read.sample_id.contains("novel"){
+                crit_2_novel += 1;
+            }
+            else{
+                crit_2_annot += 1;
+            }
+            criteria_2.entry(read.sample_id).or_insert(read.chunk);  
+
         }
     }
 
-    fs::write(candidates_path, candidates_outline);
+    //println!("Novel w/ 3'/5' AS: {}", crit_2_novel);
+    //println!("Annot w/ 3'/5' AS: {}", crit_2_annot);
+
+    criteria_2
 
 }
