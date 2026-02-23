ae94eed0a259e57ebee08800b85964b59133c523
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2026-02-23 15:48:42 -0500

    More efficient implementation

--- src/main.rs
+++ src/main.rs
@@ -33,22 +33,21 @@ fn main() {
     env::set_var("RUST_BACKTRACE", "1");
     
     //Read in and convert the reference transcriptome into kmer sketches
-    let ref_data = sketch::read_input(t, &ref_file);
-    let ref_sketch:Arc<RwLock<HashMap<String, Sketch>>>   = sketch::get_sketches(ref_data, k, s, t);  
+    let (ref_data, transcript_names) = sketch::read_input(t, &ref_file, true);
+    let ref_sketch:Arc<RwLock<HashMap<i32, Sketch>>>  = sketch::get_sketches(ref_data, k, s, t);  
     //Read in RNAseq sample
-    let sample_data = sketch::read_input(t, &sample_file);
+    let (sample_data, query_names) = sketch::read_input(t, &sample_file, false);
     let sample_data_shared = Arc::new(RwLock::new(sample_data));
     //Psuedo-align RNAseq reads to reference transcriptome    
-    let selected = map::find_ref_matches(output.clone(), ref_sketch, Arc::clone(&sample_data_shared), b, t, n, k, s, m, e as f32, B, c, mode.clone(), l, z);       
+    let selected = map::find_ref_matches(output.clone(), ref_sketch, Arc::clone(&sample_data_shared), b, t, n, k, s, m, e as f32, B, c, mode.clone(), l, z, &transcript_names);       
     //Output the filtered reads
     let output_file = output.clone() + "." + &mode + ".fa";
     let output_path = Path::new(&output_file);
     let mut output_line = "".to_string();
     let sample = sample_data_shared.read().unwrap();
     for (id, chunk) in selected.read().unwrap().iter(){
-        output_line.push_str(&(">".to_string() + &id.clone() + "\n" + &sample[*chunk].get(id).expect("msg") + "\n"));
+        output_line.push_str(&(">".to_string() + &id.to_string() + "\n" + &sample[*chunk].get(id).expect("msg") + "\n"));
     }
     fs::write(output_path, output_line); 
     
     }
-
