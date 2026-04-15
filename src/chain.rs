389f1f1e864ba2bf501607366c5135d0a6a7086a
mboro2000 <145715100+mboro2000@users.noreply.github.com>
2026-04-15 19:34:17 -0400

    Update chain.rs

--- src/chain.rs
+++ src/chain.rs
@@ -1,93 +1,61 @@
+
 use crate::types::*;
 use std::collections::HashMap;
-use std::cmp;
-use std::{thread, time};
 
 //Finds the largest set of colinear matches between a query and reference sketch
-pub fn kmer_chain(q_sketch:&HashMap<i64, Vec<i32>>, r_sketch:&HashMap<i64, Vec<i32>>, m:i32) -> (i32, i32, (i32, i32), (i32, i32)){    
-    let mut g_add:Vec<DpCell> = Vec::new();  
-    let mut g_not:Vec<DpCell> = Vec::new();  
+pub fn kmer_chain_unique(q_sketch:&HashMap<i64, Vec<i32>>, r_sketch:&HashMap<i64, Vec<i32>>, m:i32) -> (i32, i32, (i32, i32), (i32, i32), usize){    
+    let mut g:Vec<DpCell> = Vec::new(); 
     let mut anchors:Vec<(i32, i32)> = Vec::new();
-    if q_sketch.len() == 0{
-        println!("No kmers in query");
-    }
+    let mut num_repeat = 0;
+    let default:Vec<i32> = Vec::new();
+
     for (kmer, pos) in q_sketch{
-        if r_sketch.contains_key(kmer){
-            for i in pos{
-                for j in r_sketch.get(kmer).expect("msg"){
-                    anchors.push((*i, *j));
-                }
-            }            
+        let ref_pos = r_sketch.get(kmer).unwrap_or_else(|| &default);
+        if (pos.len() == 1) && (ref_pos.len() == 1){
+            anchors.push((pos[0], ref_pos[0]));
+        }
+        else if ref_pos.len() > 0{
+            num_repeat += ref_pos.len() + 1;
         }
     }     
 
-    anchors.sort_by(|a, b| a.0.cmp(&b.0).then_with(|| a.1.cmp(&b.1)));      
+    if anchors.len() == 0{
+        num_repeat = 0;
+        for (kmer, pos) in q_sketch{
+            let ref_pos = r_sketch.get(kmer).unwrap_or_else(|| &default);
+            if ref_pos.len() > 0{
+                anchors.push((pos[0], ref_pos[0]));
+            }
+        }
+    }
+
+    anchors.sort_by(|a, b| a.0.cmp(&b.0).then_with(|| a.1.cmp(&b.1)));   
     for i in 0.. anchors.len() as i32{
-        let mut add_cell:DpCell = build_cell(1, 0, i, i);
-        let mut not_cell:DpCell = build_cell(0, 0, -1, -1);
+        let mut cell:DpCell = build_cell(1, 0, i, i);
         if i == 0{            
-            g_add.push(add_cell);
-            g_not.push(not_cell);
+            g.push(cell);
         }
         else{
-            let mut unique_idx = 0;
-            let mut l = 0;
-            while unique_idx < m{
+            for l in 0..m{
                 let j = i - l - 1;
-                l += 1;
                 if j >= 0{
-                    let j = j as usize;
-                    if anchors[j].0 != anchors[j+1].0 && anchors[j].1 != anchors[j+1].1{
-                        unique_idx += 1;
-                    }
-                    //Determines optimal chain where anchor i is not included
-                    if g_not[j].OptChain_score >= not_cell.OptChain_score{
-                        not_cell = copy_cell(&g_not[j]);                 
-                    }  
-                    if g_add[j].OptChain_score >= not_cell.OptChain_score{
-                        not_cell = copy_cell(&g_add[j]);                      
-                    }                
+                    let j = j as usize;             
                     //Determines optimal chain where anchor i is included
-                    let prev = g_add[j].last_anchor;
+                    let prev = g[j].last_anchor;
                     if prev != -1{
                         let prev = prev as usize;
                         if (anchors[prev].1 < anchors[i as usize].1) && (anchors[prev].0 < anchors[i as usize].0){
                             let gap = ((anchors[prev].1 - anchors[i as usize].1) - (anchors[prev].0 - anchors[i as usize].0)).abs();
-                            if g_add[j].OptChain_score + 1 >= add_cell.OptChain_score{
-                                add_cell = build_cell(g_add[j].OptChain_score + 1, std::cmp::max(gap, g_add[j].opt_gap as i32), g_add[j].first_anchor, i);                        
-                            }                       
+                            if g[j].OptChain_score + 1 >= cell.OptChain_score{
+                                cell = build_cell(g[j].OptChain_score + 1, std::cmp::max(gap, g[j].opt_gap as i32), g[j].first_anchor, i);                        
+                            }                   
                         }
                     }
-                    let prev = g_not[j].last_anchor;
-                    if prev != -1{
-                        let prev = prev as usize;
-                        if (anchors[prev].1 < anchors[i as usize].1) && (anchors[prev].0 < anchors[i as usize].0){
-                            let gap = ((anchors[prev].1 - anchors[i as usize].1) - (anchors[prev].0 - anchors[i as usize].0)).abs();                 
-                            if g_not[j].OptChain_score + 1 >= add_cell.OptChain_score{
-                                add_cell = build_cell(g_not[j].OptChain_score + 1, std::cmp::max(gap, g_not[j].opt_gap as i32), g_not[j].first_anchor, i);                        
-                            }                       
-                        }
-                    }
-                }
-                else{
-                    unique_idx = m;
                 }
             }
-            
-            g_add.push(add_cell);
-            g_not.push(not_cell);
+            g.push(cell);
         }
     }    
-    let ret_add = g_add.pop().expect("msg");
-    let ret_not = g_not.pop().expect("msg");
-    if ret_add.OptChain_score > ret_not.OptChain_score{
-        let first_anchor = anchors[ret_add.first_anchor as usize];
-        let last_anchor = anchors[ret_add.last_anchor as usize];
-        (ret_add.OptChain_score, ret_add.opt_gap, first_anchor, last_anchor)
-    }
-    else{
-        let first_anchor = anchors[ret_not.first_anchor as usize];
-        let last_anchor = anchors[ret_not.last_anchor as usize];
-        (ret_not.OptChain_score, ret_not.opt_gap, first_anchor, last_anchor)
-    }
+    let ret = g.pop().expect("msg");    
+    (ret.OptChain_score, ret.opt_gap, anchors[ret.first_anchor as usize], anchors[ret.last_anchor as usize], num_repeat)
 }
